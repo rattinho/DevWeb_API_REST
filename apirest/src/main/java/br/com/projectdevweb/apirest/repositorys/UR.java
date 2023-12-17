b741a460515498c3ad1a3dd0b63207e4435aa7b0
Felipe Ratto <felipeguimaraesratto@gmail.com>
2023-12-17 20:12:42 -0300

    Cleaned Versioon

--- apirest/src/main/java/br/com/projectdevweb/apirest/repositorys/UR.java
+++ apirest/src/main/java/br/com/projectdevweb/apirest/repositorys/UR.java
@@ -2,7 +2,6 @@ package br.com.projectdevweb.apirest.repositorys;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import br.com.projectdevweb.apirest.models.User;
 
 @Repository
